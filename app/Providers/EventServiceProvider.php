946275dad8db62c839f8536e7d4097d5c2c68eb1
Allen <807590643@.com>
2019-11-14 19:03:45 +0800

    密码重设

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Auth\Events\Verified;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,6 +19,10 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        Verified::class => [
+            \App\Listeners\EmailVerified::class,
+        ],
+
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Auth::routes();
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+
